dfa61d93e9b58402608c48b7ff9a68a47becb962
ouzhuowei <59473281+ouzhuowei@users.noreply.github.com>
2023-07-03 17:57:41 +0800

    Upload by PicGo

---
+++ img/setSysInfo.sh
@@ -0,0 +1,26 @@
+sysctl -w net.core.somaxconn=32768
+sysctl -w net.ipv4.tcp_max_syn_backlog=16384
+sysctl -w net.core.netdev_max_backlog=16384
+
+sysctl -w net.ipv4.ip_local_port_range='1000 65535'
+
+sysctl -w net.core.rmem_default=262144
+sysctl -w net.core.wmem_default=262144
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w net.core.optmem_max=16777216
+
+#sysctl -w net.ipv4.tcp_mem='16777216 16777216 16777216'
+sysctl -w net.ipv4.tcp_rmem='1024 4096 16777216'
+sysctl -w net.ipv4.tcp_wmem='1024 4096 16777216'
+
+sysctl -w net.netfilter.nf_conntrack_max=1000000
+sysctl -w net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
+
+sysctl -w net.ipv4.tcp_max_tw_buckets=10000
+
+# 注意：不建议开启该设置，NAT 模式下可能引起连接 RST
+# sysctl -w net.ipv4.tcp_tw_recycle=1
+# sysctl -w net.ipv4.tcp_tw_reuse=1
+
+sysctl -w net.ipv4.tcp_fin_timeout=15
